ede9fa58129aa4790f4009dc80d3987fb9e87f35
alexander-n8hgeg5e <alexander.w.email@gmail.com>
2019-12-20 22:41:01 +0100

    * fix iowait calc

--- pylib/run_utils/run.py
+++ pylib/run_utils/run.py
@@ -4,6 +4,10 @@ from time import sleep
 from pylib.du import dd
 from types import FunctionType
 from os.path import sep as psep
+from os import getpgid,killpg,setpgid,getppid,getpid,kill
+from psutil import pids
+from warnings import warn
+#from pprint import pprint
 
 debug=False
 
@@ -55,50 +59,149 @@ class ConditionRunner():
         self.niceness=niceness
         self.popenargs=popenargs
         self.popenkwargs=popenkwargs
+        if 'start_new_session' in self.popenkwargs.keys():
+            warn(   
+                    "WARNING: the kwarg \"start_new_session\" is set to True\n"
+                    "in all cases."
+                )
+        self.popenkwargs.update({'start_new_session':False})
         self.polltime=polltime
         self.state=None
+        self.pid=getpid()
+        self.pgid=None
+        self.checkstats={'lastchecks':[],'last_periodes':[],'avg_period':None}
+        self.interp_counter=0
+        self.interp_interv=5
+    def _update_pids_(self):
+        self.pids=[]
+        for pid in pids():
+            try:
+                pgid=getpgid(pid)
+            except ProcessLookupError:
+                continue
+            if pgid==self.pgid and not pid==self.pid:
+                self.pids.append(pid)
+    def _stop_pids_(self):
+        if debug:
+            print("sending SIGSTOP...")
+        self._update_pids_()
+        poppids=[]
+        for pid in self.pids:
+            try:
+                kill(pid,SIGSTOP)
+            except ProcessLookupError:
+                poppids.append(pid)
+        self.state=self.STOPPED
+        for pid in poppids:
+            self.pids.remove(poppids)
+    def _start_pids_(self):
+        if debug:
+            print("sending SIGCONT...")
+        for pid in self.pids:
+            try:
+                kill(pid,SIGCONT)
+            except ProcessLookupError:
+                warn("process vanished: "+str(pid))
+                pass
+        self.state=self.STARTED
+    def check_wrapper(self,checkfunc):
+        self.interp_counter+=1
+        if debug:
+            pprint(self.checkstats)
+        lc=self.checkstats['lastchecks']
+        if len(lc) < 25:
+            check_result=checkfunc()
+            self.checkstats['lastchecks'].append(check_result)
+            return check_result
+        else:
+            if len(lc) > 26:
+                self.lastchecks.checkstats=lc[-26:-1]
+            # update periodes
+            change=[None]*3
+            j=0
+            while not change[-1] == True and change[-3] == True:
+                for i in range(1,len(lc)):
+                    if lc[i] != lc[0]:
+                        change.append(i)
+                        break
+                lc=lc[i:]
+                j+=1
+
+            if change[-1] == True and change[-3] == True:
+                self.checkstats['last_periodes'].append(change[2]-change[0])
+                self.checkstats['lastchecks']=self.checkstats['lastchecks'][1:]
+            
+            # update average periodes
+            lp=self.checkstats['last_periodes']
+            if len(lp) > 26:
+                self.lastchecks['periodes']=lp[-26:-1]
+            if len(lp) > 24:
+                # update avg_periodes
+                avgp=self.checkstats['avg_period']
+                avg_periodes=sum(lp)/len(lp)
+                self.checkstats['avg_period']=avg_periodes
+                
+                last_change=None
+                # get last change
+                lc=self.checkstats['lastchecks']
+                for i in range(-2,-len(lc)-1,-1):
+                    if lc[i] != lc[-1]:
+                        last_change=len(lc)-i
+                        break
+                if not self.interp_counter % self.interp_interv == 0 \
+                        and not last_change is None \
+                        and last_change == round(self.checkstats['avg_period']):
+                    self.checkstats['lastchecks'].append(True)
+                    if debug:
+                        print("infering check result: True")
+                    return True
+                else:
+                    check_result=checkfunc()
+                    self.checkstats['lastchecks'].append(check_result)
+                    return(check_result)
+                
     def run(self):
         cmd=['nice',"-n"+str(self.niceness),'ionice',"-"+self.ioprio]+self.cmd
         self.p=Popen(cmd,*self.popenargs,**self.popenkwargs)
+        self.pgid=getpgid(self.p.pid)
         while self.p.poll() is None:
             # is running, not done
             startit=True
             for check in self.conditions:
-                if debug:
-                    print("check...")
                 if check():
-                    self.p.send_signal(SIGSTOP)
-                    self.state=self.STOPPED
+                    self._stop_pids_()
                     startit=False
-            if startit:
-                self.p.send_signal(SIGCONT)
-                self.state=self.STARTED
+            if startit and self.state == self.STOPPED:
+                self._start_pids_()
             sleep(self.polltime)
+    def __del__(self):
+        self.p.terminate()
+        self.p.kill()
 
-def get_cpu_stats():
-    field_names=[ 'user', 'nice', 'system', 'idle', 'iowait', 'irq', 'softirq', 'steal', 'guest', 'guest_nice' ]
-    with open("/proc/stat") as f:
-        data=f.read()
-    lines=data.split("\n")
-    for line in lines:
-        if line[:4] == "cpu ":
-            cpustat_line=line[4:].strip()
-            break
-    cpustat_fields=cpustat_line.split(" ")
-    cpustats={}
-    for name,field in zip(field_names,cpustat_fields):
-        cpustats.update({name:int(field)})
-
-    _sum=sum(cpustats.values())
-    for k,v in cpustats.items():
-        cpustats.update({k:v/_sum})
-    cpustats.update({'sum':_sum})
-    return cpustats
-
-def get_iowait():
-    return get_cpu_stats()['iowait']
+#def get_cpu_stats():
+#    field_names=[ 'user', 'nice', 'system', 'idle', 'iowait', 'irq', 'softirq', 'steal', 'guest', 'guest_nice' ]
+#    with open("/proc/stat") as f:
+#        data=f.read()
+#    lines=data.split("\n")
+#    for line in lines:
+#        if line[:4] == "cpu ":
+#            cpustat_line=line[4:].strip()
+#            break
+#    cpustat_fields=cpustat_line.split(" ")
+#    cpustats={}
+#    for name,field in zip(field_names,cpustat_fields):
+#        cpustats.update({name:int(field)})
+#
+#    _sum=cpustats['user']+cpustats['system']+cpustats['iowait']+cpustats['idle']
+#    for k,v in cpustats.items():
+#        cpustats.update({k:v/_sum})
+#    cpustats.update({'sum':_sum})
+#    return cpustats
 
 def gen_max_iowait_checker(max_factor):
+    from psutil import cpu_times_percent
+    def get_iowait():
+        return cpu_times_percent().iowait
     def max_iowait_checker():
         if debug:
             print("iowait checker is checking iowait...",end="")
