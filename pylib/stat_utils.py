88e499490ce78c052cfb1f7e6035f63227280d39
alexander-n8hgeg5e <alexander.w.email@gmail.com>
2019-12-22 15:25:41 +0100

    * fix: missing selfs

--- pylib/stat_utils.py
+++ pylib/stat_utils.py
@@ -1,9 +1,16 @@
 # coding: utf-8
 from psutil import pids as get_pids
 from warnings import warn
-from os import stat
+from os import stat,kill
 from pwd import getpwuid
 from time import sleep
+from math import nan
+from pylib.du import ptb
+from random import choice
+from signal import SIGCONT,SIGSTOP
+from pprint import pprint
+from subprocess import check_call
+from sys import stderr
 debug=True
 if debug:
     from pylib.du import dd,d1,d0
@@ -32,10 +39,13 @@ class Proc_stat_getter():
         else:
             pids=[single_pid]
         stats=[]
+        #        
         for pid in pids:
             try:
                 with open('/proc/'+str(pid)+'/stat') as f:
-                    _stats=int(f.read().split()[num])
+                    _stats=f.read().split()[num]
+                    if hasattr(_stats,'__int__'):
+                        _stats=int(_stats)
                     stats.append((pid,_stats))
             except FileNotFoundError as e:
                 warn(str(e))
@@ -48,11 +58,11 @@ class Proc_stat_getter():
         except IndexError as e:
             warn(str(e))
         
-        s=sum(x[1] for x in stats)
+        s=sum((x[1] if hasattr(x[1],'__int__') else nan ) for x in stats)
         
         for i in range(len(stats)):
             pid,v = stats[i]
-            stats[i]=pid,v,v/s
+            stats[i]=pid,v,(v if hasattr(v,'__int__') else nan)/s
 
         return stats
     
@@ -96,22 +106,27 @@ def gen_max_iowait_checker(max_factor):
     return max_iowait_checker
 
 class Pid_throttler():
-    tr_methods=['renice','stop','ionice']
+    """
+    """
+    #tr_methods=['renice','stop','ionice']
+    tr_methods=['stop']
     tr_levels=['zero','low','medium','high','full']
 
     def __init__(self):
         self.pids=[]
         self.pid_data={}
         self.psg=Proc_stat_getter()
+        self.need_restore_sigcont=[]
+        self.need_restore_sigcont_cmds=[]
+        self.need_restore_sigcont_cmds_sudo=[]
 
-    def throttle(self,level):
+    def throttle(self,level,pretend=True):
         for pid in self.pids:
-            self._throttle_pid(pid)
+            self._throttle_pid(pid,level,pretend=pretend)
 
     def add_pid(self,pid,methods=['stop','renice'],limits={'maxnice': 19}):
         self.pids.append(pid)
         pid_data={}
-        pid_d
         self.pid_data.update(   {
                                  str(pid) : {
                                             'methods': methods ,
@@ -121,43 +136,146 @@ class Pid_throttler():
                             )
 
     def _backup_pid_data(self,pid):
-        stats=self.psg.get_stats('niceness','state',single_pid=pid)
-        niceness=stats['niceness'][0][1]
+        stats=self.psg.get_stats('nice','state',single_pid=pid)
+        niceness=stats['nice'][0][1]
         state=stats['state'][0][1]
-        running_state = True if not state in "SsZz" else False
         
         self.pid_data[pid].update   (
                                     {
                                         'backup':
                                                     {
                                                         'niceness':niceness,
-                                                        'running':running_state,
+                                                        'state':state,
                                                     },
                                     },
                                     )
 
-    def _throttle_pid(self,pid):
+    def _throttle_pid(self,pid,level,pretend=True):
         if not 'backup' in self.pid_data[str(pid)].keys():
             self._backup_pid_data(str(pid))
         for m in self.tr_methods:
             if m in self.pid_data[str(pid)]['methods']:
-                getattr(self,"tr_method_"+m)(level,pid)
+                getattr(self,"tr_method_"+m)(level,pid,pretend=pretend)
 
-    def tr_method_renice(self,level,pid):
+    def tr_method_renice(self,level,pid,pretend=True):
         limits={'max':19, 'min': -19}
         limits.update(self.pid_data[str(pid)]['limits'])
-        print("tr_renice",level,limits,pid)
         
-    def tr_method_stop(self,level,pid):
-        print("tr_stop",level,limits,pid)
+    def tr_method_stop(self,level,pid,pretend=True):
+        def gs(stat):
+            try:
+                s=self.controller.psg.get_stats(stat,single_pid=pid)[stat]
+            except FileNotFoundError as e:
+                print(e)
+            ret=(s[0] if len(s)  > 0 else s )
+            ret=(ret[1] if len(ret)  > 1 else ret )
+            return str(ret)
+        print("tr_stop level="+str(level)+" pid="+ str(pid)+" cmd="+str(gs('comm')))
+        try:
+            print   (
+                    "delayacct_blkio_ticks="+gs('delayacct_blkio_ticks')+"\n"
+                    "uid="+str(self.controller._get_uid(pid))+"\n"
+                    'state='+gs('state')
+                    )
+        except FileNotFoundError as e:
+            print(e)
+        def check(level):
+            return choice((level*[True])+[False,False,False])
+        try:
+            if check(level):
+                if pretend:
+                    print("would send SIGSTOP to pid",pid)
+                    print()
+                else:
+                    self._stop_pid(pid)
+            else:
+                if pretend:
+                    print("would send SIGCONT to pid:",pid)
+                    print()
+                else:
+                    kill(pid,SIGCONT)
+        except ProcessLookupError:
+            pass
+    
+        # def gs(stat):
+        #     s=self.controller.psg.get_stats(stat,single_pid=pid)[stat]
+        #     return str(s[0][1])
+        # print("tr_stop level="+str(level)+" pid="+ str(pid)+" cmd="+str(gs('comm')))
 
-    def tr_method_ionice(self,level,pid):
-        print("tr_stop",level,limits,pid)
+    def _stop_pid(self,pid):
+        try:
+            check_call(['kill','-V'])
+            cmd=['kill','-sSIGCONT',str(pid)]
+            sudocmd=['sudo']+cmd
+
+            if pid not in self.need_restore_sigcont:
+                a=True
+            if cmd not in self.need_restore_sigcont_cmds:
+                b=True
+            if sudocmd not in self.need_restore_sigcont_cmds_sudo:
+                c=True
+
+            with open('/proc/'+str(pid)+'/stat','rb',buffering=0) as f:
+                data=b''
+                while True:
+                    data+=f.read(10)
+                    fields=data.split(" ")
+                    if len(fields) >= 4:
+                        break
+                if not fields[2] in b'TZX':
+                    kill(pid,SIGSTOP)
+                    self.need_restore_sigcont.append(pid)
+                    self.need_restore_sigcont_cmds.append(cmd)
+                    self.need_restore_sigcont_cmds_sudo.append(sudocmd)
+        except Exception as e:
+            print(e,file=stderr)
+        
+    def tr_method_ionice(self,level,pid,pretend=True):
+        pass
+        # def gs(stat):
+        #     s=self.controller.psg.get_stats(stat,single_pid=pid)[stat]
+        #     return str(s[0][1])
+        # print("tr_ionice level="+str(level)+" pid="+ str(pid)+" cmd="+str(gs('comm')))
+
+    def _cleanup(self):
+        try:
+            for pid in self.need_restore_sigcont:
+                try:
+                    kill(pid,SIGCONT)
+                except Exception as e:
+                    print(e)
+        except Exception as e:
+            print(e)
+            try:
+                for cmd in self.need_restore_sigcont_cmds:
+                    try:
+                        check_call(cmd)
+                    except Exception as e:
+                        print(e)
+            except Exception as e:
+                print(e)
+                try:
+                    for sudocmd in self.need_restore_sigcont_cmds_sudo:
+                        try:
+                            check_call(sudocmd)
+                        except Exception as e:
+                            print(e)
+                except Exception as e:
+                    print(e)
+
+    def __del__(self):
+        self._cleanup()
 
 class IO_wait_controller():
-    exclude_stats={}
-    limit_stats={}
-    method_restrict_stats={}
+    exclude_stats=  {
+                    'uid' :
+                            [
+                            0,
+                            1000,
+                            ],
+                    }
+    limit_stats={'uid':{'limits':{},"vals":[]}}
+    method_restrict_stats={'uid':{'methods':{},'vals':[]},'delayacct_blkio_ticks':{'methods':{},'vals':[]}}
 
     sleeptime=5
 
@@ -167,15 +285,16 @@ class IO_wait_controller():
         self.throtte_range=10
         self.thr_level=self.pthr.tr_levels.index("zero")
         self.psg=Proc_stat_getter()
+        self._gen_stat_keys()
 
-    def _get_uid(pid):
+    def _get_uid(self,pid):
         return stat('/proc/'+str(pid)).st_uid
 
-    def _add_exclude_stat(name,value_or_value_regex):
+    def _add_exclude_stat(self,name,value_or_value_regex):
         ex={self.struct_proc_pid_stat.index(name) : value_or_value_regex}
         self.exclude_stats.update(ex)
 
-    def _add_exclude_user(uid_or_name):
+    def _add_exclude_user(self,uid_or_name):
         if type(uid_or_name) is str:
             uid=getpwnam(uid_or_name).pw_uid
         elif hasattr(uid_or_name,'__int__'):
@@ -188,6 +307,27 @@ class IO_wait_controller():
             l.append((pid,self._get_uid(pid)))
         return l
 
+    def _gen_stat_keys(self):
+        sls=self.limit_stats
+        ses=self.exclude_stats
+        smrs=self.method_restrict_stats
+        _keys=list(sls)
+        _keys+=list(ses)
+        _keys+=list(smrs)
+        keys=[]
+        for k in _keys:
+            if not k in keys:
+                keys.append(k)
+        self.stat_keys=keys
+        keys_psg=keys.copy()
+        keys_psg.remove('uid')
+        self.stat_keys_psg=keys_psg
+
+    def _get_stats_single_pid(self,pid):
+        stats=self.psg.get_stats(*self.stat_keys_psg,single_pid=pid)
+        stats.update({'uid':[(pid,self._get_uid(pid),None)]})
+        return stats
+
     def _check_pid(self,pid):
         """
         return dict with,
@@ -197,46 +337,70 @@ class IO_wait_controller():
         limits={}
         methods=self.pthr.tr_methods
         exclude=False
-        if self._get_uid(pid) in self.exclude_uids:
-            exclude=True
-        keys=list(self.exclude_stats.keys())
-        stats=self.psg.get_stats(*keys,single_pid=pid)
-        stats.update({'uid':[(pid,self._get_uid(pid))]})
+        stats=self._get_stats_single_pid(pid) 
+        sls=self.limit_stats
+        ses=self.exclude_stats
+        smrs=self.method_restrict_stats
         for k,v in stats.items():
-            for _pid,_v in v:
+            #            
+            #            
+            for _pid,_v,_vp in v:
                 if _pid == pid:
-                    if _v == self.exclude_stats[k]:
+                    if k in ses and _v in self.exclude_stats[k]:
                         exclude=True
                         # this pid is fully excluded
                         break
 
                     # if _statval matches requested2limit val
-                    if _v == self.limit_stats[k]['val']:
+                    if k in sls and _v in self.limit_stats[k]['vals']:
                         limits.update(self.limit_stats[k]['limits'])
 
                     # if _statval matches requested2restrict method val
-                    if _v == self.method_restrict_stats[k]['val']:
+                    if k in smrs and _v in self.method_restrict_stats[k]['vals']:
                         methods.update(self.method_restrict_stats[k]['methods'])
         return {'exclude':exclude,'methods':methods,'limits':limits}
-        
-    def run(self):
-        while True:
-            if self.iowait_checker():
-                dd("check True")
-                data={}
-                data.update(self.psg.get_stats("delayacct_blkio_ticks"))
-                for i in range(min(self.throtte_range,len(data["delayacct_blkio_ticks"]))):
-                    check_data=self._check_pid(str(data["delayacct_blkio_ticks"][i][0]))
-                    if not checkdata['exclude']:
-                        m=check_data['methods']
-                        limits=check_data['limits']
-                        self.pthr.add(pid,methods=m,limits=limits)
-                self.thr_level+=1
-            else:
-                self.thr_level-=1
+    
+    def _check_add(self,pid):
+        check_data=self._check_pid(pid)
+        if not check_data['exclude']:
+            m=check_data['methods']
+            limits=check_data['limits']
+            self.pthr.add_pid(pid,methods=m,limits=limits)
+        elif debug:
+            excluding_pid=str(pid)
+            dd(excluding_pid)
+            uid=str(self._get_uid(pid))
 
-            self.pthr.throttle(self.thr_level)
-            sleep(self.sleeptime)
+    def run(self,pretend=True):
+        try:
+            while True:
+                if self.iowait_checker():
+                    self.thr_level+=1
+
+                    data=self.psg.get_stats("delayacct_blkio_ticks")
+                    data.update(self.psg.get_stats("state"))
+
+                    if debug:
+                        data_debug=self.psg.get_stats("comm")
+
+                    #for i in range(min(self.throtte_range,len(data["delayacct_blkio_ticks"]))):
+                    #    pid=data['delayacct_blkio_ticks'][i][0]
+                    #    self._check_add(pid)
+                    for s in data['state']:
+                        if s[1]in "DT":
+                            self._check_add(s[0])
+
+                else:
+                    self.thr_level-=1
+                self.thr_level=min(max(0,self.thr_level),4)
+                self.pthr.controller=self
+                self.pthr.throttle(self.thr_level,pretend=pretend)
+                sleep(self.sleeptime)
+        except Exception as e:
+            ptb(e)
+            self.pthr.__del__()
+            del(self.pthr)
+
 
                             
-# vim: set foldlevel=0 foldmethod=indent :
+# vim: set foldlevel=0 foldmethod=indent foldnestmax=3 :
