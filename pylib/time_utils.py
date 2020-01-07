05d160f43a92f5c42cba343a390a577cac7ee531
alexander-n8hgeg5e <alexander.w.email@gmail.com>
2020-01-07 14:46:07 +0100

    * add selectable seperator, req for tmux session name

--- pylib/time_utils.py
+++ pylib/time_utils.py
@@ -1,6 +1,6 @@
 from time import localtime,time
 
-def get_useful_localtime_string(decimal_sec_digits=2):
+def get_useful_localtime_string(decimal_sec_digits=2,dec_sec_sep="."):
     """
     Returns 12 chars long, easy to read and comparable time string.
     Format reading: century , day of year, second of day
@@ -16,7 +16,7 @@ def get_useful_localtime_string(decimal_sec_digits=2):
     sec_of_day  = t.tm_hour*3600+t.tm_min*60+t.tm_sec
     if decimal_sec_digits > 0:
         dec_sec = int(round(time() % 1,decimal_sec_digits)*10**decimal_sec_digits)
-        dec_sec_str = (".{:0>"+str(decimal_sec_digits)+"d}").format(dec_sec)
+        dec_sec_str = (dec_sec_sep+"{:0>"+str(decimal_sec_digits)+"d}").format(dec_sec)
     else:
         dec_sec_str = ""
     # format string, pad the fields left side with "_"
