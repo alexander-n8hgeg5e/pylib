4fd48117cec9a2c3e6d8b45b1732f19dd110c2d1
alexander-n8hgeg5e <alexander.w.email@gmail.com>
2020-04-07 14:49:16 +0200

    add v2 version of function ...
    get_connected_output_count_at_env_layout_index

--- pylib/screen_utils/screen_utils.py
+++ pylib/screen_utils/screen_utils.py
@@ -63,6 +63,45 @@ def get_connected_output_count_at_env_layout_index(position_index,layout=None,re
     # if not found
     return None
 
+def get_connected_output_count_at_env_layout_index_v2(position_index,layout=None,return_x_display=False):
+    """
+    returns the number of the xrandr list , start is 0.
+    returns tuple with second xdisplay if return_x_display
+    a xrandr list belongs to a x_display.
+    the env layout is the base for this func to get information
+    it defines the overall layout.
+    the env layout can contain more than one xserver
+    each of them with multiple outputs
+    implies that the outputs and the env_layout
+    is physically sorted in the order of their x-positions
+    and in the same direction.
+    the env-layout is meant to be from left to right anyways.
+    This means for example, if output has higher x pos than another
+    it has to be right of the another.
+    It also would work if it is left of the another but
+    then the layout has to be defined from left
+    to right.
+    """
+    if not layout:
+        env_layout=parse_screen_layout_env_var_v2()
+    x_display=env_layout[position_index]['x_server']
+    outputs=get_connected_outputs(x_display=x_display)
+    # the pos is a position on one x-server
+    pos=0
+    for j in range(position_index+1):
+        if env_layout[j]['x_server'] == x_display:
+            for i in range(len(outputs)):
+                    x=int(outputs[i]['pos'][0])
+                    #print("pos_index:",j,"oup_pos:",x,'env_pos:',pos)
+                    if pos==x and j==position_index:
+                        if return_x_display:
+                            return i , x_display
+                        else:
+                            return i
+            pos+=int(env_layout[j]['dim'][0])
+    # if not found
+    return None
+
 def env_screen_layout_2_region_layout(layout=None,x_display=None):
     """
     assumes that regions are in proper order,
