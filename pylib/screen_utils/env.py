0cb3a21ef406ab4c0aee7875f9c90eecd95e7697
alexander-n8hgeg5e <alexander.w.email@gmail.com>
2020-04-29 18:11:35 +0200

    add v3 version that converts number strings to int

--- pylib/screen_utils/env.py
+++ pylib/screen_utils/env.py
@@ -40,4 +40,24 @@ def parse_screen_layout_env_var_v2():
         screen_layout.append(layout)
     return screen_layout
 
+def parse_screen_layout_env_var_v3():
+    v=environ['screen_layout_v2']
+    screens=v.split('##')
+    screen_layout=[]
+    for screen in screens:
+        x_server,geo = screen.split("#")
+        x,y,size = geo.split('_')
+        w,h=size.split('x')
+        geo=[]
+        for i in [x,y,w,h]:
+            geo.append(int(i))
+        geo={
+                'pos' : (int(x),int(y))  ,
+                'dim' : (int(w),int(h))  ,
+            }
+        layout={'x_server' : x_server }
+        layout.update(geo)
+        screen_layout.append(layout)
+    return screen_layout
+
 # vim: set foldmethod=indent foldlevel=0 foldnestmax=1 :
