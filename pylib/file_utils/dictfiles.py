5248f4240972aeb126e28eae681960dafddc72f2
alexander-n8hgeg5e <alexander.w.email@gmail.com>
2020-12-26 02:00:16 +0100

    add read write "dictfiles" functions

---
+++ pylib/file_utils/dictfiles.py
@@ -0,0 +1,52 @@
+from os import mkdir
+from os.path import exists
+from os import listdir
+from os.path import isfile,isdir
+from re import match
+from pprint import pprint
+from collections import OrderedDict as Od
+
+def write(d,path=".",max_data_len=64,silent_skip=False):
+    for k,v in d.items():
+        if not type(v) is dict:
+            data=str(v).encode()
+            if len(data) > max_data_len and not silent_skip:
+                raise Exception("ERROR: data len limit reached. file is too big. \n file=\"{}\"".format(k))
+            with open(path+"/"+k,mode="wb") as f:
+                f.write(data[:max_data_len])
+        else:
+            path=path+"/"+k
+            if not exists(path):
+                mkdir(path)
+            write(v,path=path)
+
+
+def read(path=".",regex=".*",max_data_len=64,silent_skip=False ,nostrip=False,noconv=False):
+
+    print("regex=",regex)
+    if regex != 'slot_1':
+        raise Exception()
+    dirlist=listdir(path)
+    l=[]
+    data={}
+    for thing in dirlist:
+        if isfile(path+"/"+thing) and match(regex,thing):
+            l.append(thing)
+        elif isdir(path+"/"+thing):
+            data.update( { thing : read( path=path + "/" + thing )},regex=regex,max_data_len=max_data_len,silent_skip=silent_skip,noconv=noconv,nostrip=nostrip)
+    l.sort()
+    for k in l:
+        with open(path+"/"+k) as f:
+            d=f.read(max_data_len + 1)
+            if len(d) > max_data_len and not silent_skip:
+                raise Exception("ERROR: data len limit reached. file is too big. \n file=\"{}\"".format(k))
+            d=d[:max_data_len]
+            if not nostrip:
+                d = d.strip()
+            if not noconv:
+                if match("^[0-9]+$",d):
+                    d = int(d)
+                elif match("^[0-9]*[.][0-9]*$",d):
+                    d = float(d)
+            data.update({k:d})
+    return data
