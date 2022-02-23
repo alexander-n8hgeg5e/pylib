416b5314c57cf49d55c36dac4dafd69d86e879bd
alexander-n8hgeg5e <alexander.w.email@gmail.com>
2022-02-23 21:03:09 +0100

    improve file2list and add more 2list functions

--- pylib/file_utils/split.py
+++ pylib/file_utils/split.py
@@ -1,8 +1,33 @@
 
 from os.path import expanduser,expandvars
 
-def file2list(f):
-    with open(expanduser(expandvars(f))) as ff:
-        kwd_data=ff.read()
-    kwd_data=kwd_data.strip("\n")
-    return kwd_data.split("\n")
+def file2list(filename,decode=True):
+    """
+    Expands filename,reads file,strips file, returns list of lines.
+    """
+    if decode:
+        with open(expanduser(expandvars(filename))) as f:
+            data=f.read()
+    else:
+        with open(expanduser(expandvars(filename)),"rb") as f:
+            data=f.read()
+    data=data.strip()  # python strips it proper 0x0a,0x20,0x0d, maybe more
+    return data.splitlines()
+
+def file2list_v2(filename,decode=False):
+    """
+    Expands filename,reads,strips all lines,pops empty,returns list of lines.
+    """
+    if decode:
+        with open(expanduser(expandvars(filename))) as f:
+            data=f.read()
+    else:
+        with open(expanduser(expandvars(filename)),"rb") as f:
+            data=f.read()
+    return data2list(data)
+
+def data2list(data):
+    data = data.splitlines()
+    data = [ line.strip() for line in data ]
+    data = [ d for d in filter(lambda x: len(x) > 0, data ) ]
+    return data
