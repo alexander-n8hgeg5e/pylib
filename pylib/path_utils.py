28f3930b83702c2e0ba78e0dfb91be9e8c5640b8
alexander-n8hgeg5e <alexander.w.email@gmail.com>
2022-08-14 19:00:09 +0200

    add function shorten_uniq

--- pylib/path_utils.py
+++ pylib/path_utils.py
@@ -11,3 +11,22 @@ def mkdir_recursive(p, mode=511, dir_fd=None):
         if not exists(want_to_exist):
             print(f"mkdir \"{want_to_exist}\"")
             mkdir(want_to_exist)
+
+def shorten_uniq(f,l=8,uniq_file_ids={}):
+    #print(f"\"{f}\" : ",end="")
+    if f in uniq_file_ids.keys():
+        #print(f"\"{uniq_file_ids[f]}\"")
+        return uniq_file_ids[f]
+    if len(f) > l:
+        sf=f[-l:]
+        loopcount=0
+        while sf in uniq_file_ids.values() and l > loopcount:
+            loopcount+=1
+            sf = sha1(f.encode()).hexdigest()[:loopcount] + f[ -l+loopcount:]
+        if sf in uniq_file_ids.values():
+            raise Exception(f"Could not find uniq file id for file :\n{f}")
+    else:
+        sf=f
+    uniq_file_ids.update({f:sf})
+    #print(f"\"{sf}\"")
+    return sf
